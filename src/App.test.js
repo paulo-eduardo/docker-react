1691d058a972ebe80639d9d27049a810b4775fad
Paulo Eduardo <pauloes.dev@gmail.com>
2022-05-15 14:04:01 -0300

    fixing tests

--- src/App.test.js
+++ src/App.test.js
@@ -9,6 +9,6 @@ test("renders learn react link", () => {
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn ract/i);
+  const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
