de35bd6c604268381682bb0cd42a4dafb3ee9f23
Paulo Eduardo <pauloes.dev@gmail.com>
2022-05-15 13:49:24 -0300

    ruining tests

--- src/App.test.js
+++ src/App.test.js
@@ -9,6 +9,6 @@ test("renders learn react link", () => {
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/learn ract/i);
   expect(linkElement).toBeInTheDocument();
 });
